bcb0f76a9a4ec2668cac99354a5c891f4b94be40
joneswang <jonescwang@gmail.com>
2025-07-20 16:20:39 -0400

    first edittext done

--- app/src/main/java/com/jovicheer/whisper_voice_android_native/MainActivity.kt
+++ app/src/main/java/com/jovicheer/whisper_voice_android_native/MainActivity.kt
@@ -5,16 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.jovicheer.whisper_voice_android_native.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 目前只有印 log，確認 EditText 可正常存取
+        binding.inputField.setOnEditorActionListener { v, _, _ ->
+            android.util.Log.d("MainActivity", "Typed: ${v.text}")
+            false
         }
     }
 }
